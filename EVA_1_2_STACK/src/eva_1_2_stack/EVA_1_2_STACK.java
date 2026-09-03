cfe19aa871869621e270ee249b2e8423a4fa2ae5
Jocelyn Zamarron Mendoza <jozzamarron@gmail.com>
2026-09-03 16:48:22 -0600

    Add files via upload

---
+++ EVA_1_2_STACK/src/eva_1_2_stack/EVA_1_2_STACK.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_2_stack;
+
+/**
+ *
+ * @author jozza
+ */
+public class EVA_1_2_STACK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("INICIA  main");
+        
+        A();
+        System.out.println("TERMINA main");
+    }
+    public static void A(){
+ System.out.println("INICIA  A");
+
+        B();
+        
+        System.out.println("TERMINA A");
+    }
+    
+    public static void B(){
+        
+        System.out.println("INICIA B");
+        int i = 5;
+        double d =5.45;
+        System.out.println("TERMINA B");
+
+
+
+
+    }
+    
+}
---
+++ EVA_1_3_STACKOVERFLOW/src/eva_1_3_stackoverflow/EVA_1_3_STACKOVERFLOW.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_3_stackoverflow;
+
+/**
+ *
+ * @author jozza
+ */
+public class EVA_1_3_STACKOVERFLOW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        A();      
+    }
+    public static void A(){
+
+        B();       
+    }
+    
+    public static void B(){
+        
+        int i = 5;
+        double d =5.45;
+        
+        A();
+       
+
+
+
+        
+
+
+    }
+    
+}
